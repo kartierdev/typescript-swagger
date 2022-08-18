664f4b7cf3350de7f8133d04c3f7d1b0ed21e7be
kartien <kevinramon523@gmail.com>
2022-08-18 13:03:52 -0500

    Create swagger routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -20,36 +20,53 @@ const router = Router();
  *        id:
  *          type: string
  *          description: the auto-generated id of task
- *        name: 
+ *        name:
  *          type: string
  *          description: the name of the task
  *        description:
  *          type: string
- *          description: the description of the task 
+ *          description: the description of the task
  *      required:
- *        - name 
+ *        - name
  *        - description
  *      example:
  *        id : o1ZHug76dW-aH_HPhqBN7
- *        name: All tasks 
- *        description: I have all tasks 
+ *        name: All tasks
+ *        description: I have all tasks
  */
 
 /**
  * @swagger
  * /tasks:
- *  get: 
+ *  get:
  *    sumary: Return a Task list
  *    responses:
  *      200:
  *        description: the list of tasks
  *        content:
  *          application/json:
- *            schema: 
+ *            schema:
  *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Task'
  */
 
 router.get("/tasks", getTasks);
+
+/**
+ * @swagger
+ * /tasks/count:
+ *  get:
+ *    sumary: Get total tasks count
+ *    responses:
+ *       200: 
+ *         description: The total number of tasks
+ *         content: 
+ *            text/plane: 
+ *               schema:
+ *                type: integer 
+ *                example: 15
+ */
 
 router.get("/tasks/count", count);
 
