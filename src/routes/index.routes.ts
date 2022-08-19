4b51dc468d5688c23deddf26b16f8cd8a252aeb3
kartien <kevinramon523@gmail.com>
2022-08-19 12:53:24 -0500

    Add routes task:id task/count

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -33,13 +33,36 @@ const router = Router();
  *        id : o1ZHug76dW-aH_HPhqBN7
  *        name: All tasks
  *        description: I have all tasks
+ *    TasksNotFound: 
+ *       type: object
+ *       properties:
+ *          msg:
+ *           type: string
+ *           description: a message not found tasks 
+ *       example:
+ *         msg: Task was not found
+ *  parameters:
+ *    taskId:
+ *      in: path
+ *      name: id
+ *      required: true
+ *      schema: 
+ *        type: string
+ *      description: The task id 
  */
 
+/**
+ * @swagger
+ * tags:
+ *  name: Tasks
+ *  description: Tasks endpoint
+ */
 /**
  * @swagger
  * /tasks:
  *  get:
  *    sumary: Return a Task list
+ *    tags: [Tasks]
  *    responses:
  *      200:
  *        description: the list of tasks
@@ -58,6 +81,7 @@ router.get("/tasks", getTasks);
  * /tasks/count:
  *  get:
  *    sumary: Get total tasks count
+ *    tags: [Tasks]
  *    responses:
  *       200: 
  *         description: The total number of tasks
@@ -67,10 +91,54 @@ router.get("/tasks", getTasks);
  *                type: integer 
  *                example: 15
  */
-
 router.get("/tasks/count", count);
 
+/**
+ * @swagger
+ * /tasks: 
+ *  post:
+ *    sumary: Create a new task
+ *    tags: [Tasks]
+ *    requestBody: 
+ *      required: true
+ *      content:
+ *         application/json:
+ *           schema: 
+ *             $ref: "#/components/schemas/Task"
+ *    responses: 
+ *      200:
+ *        description: The successful createTask 
+ *        content: 
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/Task"
+ *      500:
+ *        description: The some error occurred
+ */
 router.post("/tasks", createTask);
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *  get: 
+ *    sumary: Get task by id 
+ *    tags: [Tasks]
+ *    parameters:
+ *      - $ref: '#/components/parameters/taskId'
+ *    responses: 
+ *      200:
+ *        description: The task was successfully retrieved
+ *        content:
+ *          application/json:
+ *             schema: 
+ *               $ref: "#/components/schemas/Task"
+ *      404:
+ *      description: The task could not be retrieved
+ *      content:
+ *        application/json:
+ *           schema: 
+ *             
+ */
 
 router.get("/tasks/:id", getTask);
 
